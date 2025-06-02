e7f411fcfa51ab2327e17d59a149937ec69855d7
tomeu <txsastre@gmail.com>
2025-06-02 14:25:08 +0200

    docker again

--- ollama/start.sh
+++ ollama/start.sh
@@ -1,2 +1,6 @@
 podman run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
 #docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+
+
+#Now you can run a model:
+#docker exec -it ollama ollama run llama3
