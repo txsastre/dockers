7e086df153a0beac7177a067ea256b2a46f884ab
tomeu <txsastre@gmail.com>
2025-06-03 12:08:17 +0200

    ollaba+web

--- open-webui/start.sh
+++ open-webui/start.sh
@@ -1,1 +1,8 @@
-docker run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
+docker run -d \
+  -p 3000:8080 \
+  --add-host=host.docker.internal:host-gateway \
+  -v open-webui:/app/backend/data \
+  -e OLLAMA_BASE_URL=http://host.docker.internal:11434 \
+  --name open-webui \
+  --restart always \
+  ghcr.io/open-webui/open-webui:main
