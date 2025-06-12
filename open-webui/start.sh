254f60b338e2df04d3d8301681bda3a0c5dea4e3
tomeu <txsastre@gmail.com>
2025-06-12 11:08:48 +0200

    fedora

--- open-webui/start.sh
+++
@@ -1,8 +0,0 @@
-docker run -d \
-  -p 3000:8080 \
-  --add-host=host.docker.internal:host-gateway \
-  -v open-webui:/app/backend/data \
-  -e OLLAMA_BASE_URL=http://host.docker.internal:11434 \
-  --name open-webui \
-  --restart always \
-  ghcr.io/open-webui/open-webui:main
